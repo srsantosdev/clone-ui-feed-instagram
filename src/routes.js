04eeb7d8e3c4bd64c94a55df87eebbdde0017114
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-03-03 19:48:24 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Header from '~/components/Header';
 
-import logo from '~/assets/instagram.png';
-
 import Feed from '~/pages/Feed';
 
 const { Navigator, Screen } = createStackNavigator();
@@ -20,20 +18,4 @@ const Routes = () => (
     <Screen name="Feed" component={Feed} />
   </Navigator>
 );
-
-/* const Routes = createAppContainer(
-  createStackNavigator(s
-    { Feed },
-    {
-      headerLayoutPreset: 'center',
-      defaultNavigationOptions: {
-        headerTitle: <Image source={logo} />,
-        headerStyle: {
-          backgroundColor: '#f5f5f5',
-        },
-      },
-    },
-  ),
-); */
-
 export default Routes;
